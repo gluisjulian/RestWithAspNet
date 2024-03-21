26005efb8dba7a3616fb46917d0bc1dfe76e40ec
Gabriel Julian <gluisjulian@gmail.com>
2024-03-21 15:28:50 -0300

    Custom Serialization.

--- RestWithAspNet/RestWithAspNet/Data/VO/PersonVO.cs
+++ RestWithAspNet/RestWithAspNet/Data/VO/PersonVO.cs
@@ -1,11 +1,22 @@
-﻿namespace RestWithAspNet.Data.Vo
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithAspNet.Data.Vo
 {
     public class PersonVO
     {
+        [JsonPropertyName("code")]
         public long Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
+
+        [JsonPropertyName("sobrenome")]
         public string LastName { get; set; }
+
+        [JsonPropertyName("endereco")]
         public string Address { get; set; }
+
+        [JsonIgnore]
         public string Gender { get; set; }
     }
 }
